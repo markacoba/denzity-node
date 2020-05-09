807a795dc2d423eb90dd78b453c27aae3a93e86f
Mark Arjay Acoba <markarjayacoba@yahoo.com>
2020-05-09 22:01:06 +0800

    init commit

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const router = express.Router();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+router.get('/test', (req, res, next) => {  
+	res.json("Connection is good.")
+});
+
+app.use('/', router);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
